fe6d01c626a02979905200227983ba88d820e5bb
Jon Heslop <jon@jonheslop.com>
2025-10-05 00:05:05 +0100

    Info page

--- src/turso.ts
+++ src/turso.ts
@@ -1,6 +1,18 @@
 import { createClient } from "@libsql/client/web";
+import { type Row } from "@libsql/client";
 
 export const turso = createClient({
   url: import.meta.env.TURSO_DATABASE_URL,
   authToken: import.meta.env.TURSO_AUTH_TOKEN,
 });
+
+export type Fave = Row & {
+  id: number;
+  created_at: string;
+  url: string;
+  author: string;
+  tags: string;
+  width: number;
+  height: number;
+  Source: string;
+};
