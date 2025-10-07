46767d52b6628dc584c439e854d57e95ff7aa0b9
Jon Heslop <jon@jonheslop.com>
2025-10-07 21:37:43 +0100

    Search box and route

---
+++ src/pages/search.ts
@@ -0,0 +1,15 @@
+export const prerender = false;
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  try {
+    const data = await request.formData();
+    console.log(data);
+    const search = data.get("search");
+    return redirect(`/search/${search}`);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+};
