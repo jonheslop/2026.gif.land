6dd0ee37a13e7a0764719f783fb2bec51b0a24fb
Jon Heslop <jon@jonheslop.com>
2025-11-24 10:15:06 +0000

    seems fine now

--- src/turso.ts
+++ src/turso.ts
@@ -2,9 +2,7 @@ import { createClient } from "@libsql/client/web";
 import { type Row } from "@libsql/client";
 
 export const turso = createClient({
-  url:
-    import.meta.env.TURSO_DATABASE_URL ??
-    "libsql://bukkit-jonheslop.aws-us-east-1.turso.io",
+  url: import.meta.env.TURSO_DATABASE_URL ?? process.env.TURSO_DATABASE_URL,
   authToken: import.meta.env.TURSO_AUTH_TOKEN ?? process.env.TURSO_AUTH_TOKEN,
 });
 
