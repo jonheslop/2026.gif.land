865c25d1dad900b0ff67920f92776fd003158c83
Jon Heslop <jon@jonheslop.com>
2025-12-01 21:55:30 +0000

    Explicitly passing config

--- src/turso.ts
+++ src/turso.ts
@@ -1,10 +1,12 @@
 import { createClient } from "@libsql/client/web";
 import { type Row } from "@libsql/client";
 
-export const turso = createClient({
-  url: import.meta.env.TURSO_DATABASE_URL ?? process.env.TURSO_DATABASE_URL,
-  authToken: import.meta.env.TURSO_AUTH_TOKEN ?? process.env.TURSO_AUTH_TOKEN,
-});
+export const turso = (
+  config = {
+    url: import.meta.env.TURSO_DATABASE_URL ?? process.env.TURSO_DATABASE_URL,
+    authToken: import.meta.env.TURSO_AUTH_TOKEN ?? process.env.TURSO_AUTH_TOKEN,
+  },
+) => createClient(config);
 
 export type Fave = Row & {
   id: number;
