98c19d81e50da5cf9fce39bf66e73bb159170021
Jon Heslop <jon@jonheslop.com>
2026-02-20 08:52:00 +0000

    API route to get json of every gif

---
+++ src/pages/api/index.ts
@@ -0,0 +1,20 @@
+export const prerender = false;
+import { turso } from "../../turso";
+
+export async function GET({}) {
+  try {
+    const { rows } = await turso().execute(
+      "SELECT * FROM favourites WHERE published = true ORDER BY url ASC",
+    );
+
+    return new Response(JSON.stringify(rows), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+}
